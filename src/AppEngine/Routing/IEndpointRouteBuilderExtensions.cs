2f596a06f0bb6e2cdf2490204cfa7673dcf5b105
Angelo Pirola <angelo9871@gmail.com>
2026-02-08 09:57:23 +0100

    Add modular endpoint mapping via routing extensions
    Introduced IEndpointRouteHandlerBuilder interface with static MapEndpoints method for endpoint registration. Added IEndpointRouteBuilderExtensions with methods to discover and map endpoint handler builders from assemblies using reflection, enabling convention-based and modular endpoint registration.

---
+++ src/AppEngine/Routing/IEndpointRouteBuilderExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Routing;
+
+namespace AppEngine.Routing;
+
+public static class IEndpointRouteBuilderExtensions
+{
+    public static void MapEndpoints(this IEndpointRouteBuilder endpoints, Func<Type, bool>? predicate = null)
+        => MapEndpoints(endpoints, Assembly.GetCallingAssembly(), predicate);
+
+    public static void MapEndpoints(this IEndpointRouteBuilder endpoints, Assembly assembly, Func<Type, bool>? predicate = null)
+    {
+        ArgumentNullException.ThrowIfNull(endpoints);
+        ArgumentNullException.ThrowIfNull(assembly);
+
+        var endpointRouteHandlerBuilderInterfaceType = typeof(IEndpointRouteHandlerBuilder);
+
+        var endpointRouteHandlerBuilderTypes = assembly.GetTypes().Where(t =>
+            t.IsClass && !t.IsAbstract && !t.IsGenericType
+            && endpointRouteHandlerBuilderInterfaceType.IsAssignableFrom(t)
+            && (predicate?.Invoke(t) ?? true));
+
+        foreach (var endpointRouteHandlerBuilderType in endpointRouteHandlerBuilderTypes)
+        {
+            var mapEndpointsMethod = endpointRouteHandlerBuilderType.GetMethod(nameof(IEndpointRouteHandlerBuilder.MapEndpoints), BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)!;
+            mapEndpointsMethod.Invoke(null, [endpoints]);
+        }
+    }
+
+    public static void MapEndpointsFromAssemblyContaining<T>(this IEndpointRouteBuilder endpoints, Func<Type, bool>? predicate = null) where T : class
+        => MapEndpoints(endpoints, typeof(T).Assembly, predicate);
+
+    public static void MapEndpoints<T>(this IEndpointRouteBuilder endpoints) where T : IEndpointRouteHandlerBuilder
+        => T.MapEndpoints(endpoints);
+}
