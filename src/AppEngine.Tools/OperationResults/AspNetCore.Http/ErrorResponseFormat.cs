e95a967879153c130dbc5423310792510610eacc
Angelo Pirola <angelo9871@gmail.com>
2026-02-10 00:10:05 +0100

    Add ASP.NET Core extensions for standardized operation results
    Introduce extension methods and configuration options to map Result and Result<T> to ASP.NET Core HTTP responses. Includes error response formatting, status code mapping, DI registration, and support for file/stream responses. All features are namespaced under AppEngine.Tools.OperationResults.AspNetCore.Http.

---
+++ src/AppEngine.Tools/OperationResults/AspNetCore.Http/ErrorResponseFormat.cs
@@ -0,0 +1,7 @@
+﻿namespace AppEngine.Tools.OperationResults.AspNetCore.Http;
+
+public enum ErrorResponseFormat
+{
+    Default,
+    List
+}
---
+++ src/AppEngine.Tools/OperationResults/AspNetCore.Http/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace AppEngine.Tools.OperationResults.AspNetCore.Http;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOperationResult(this IServiceCollection services, Action<OperationResultOptions>? configuration = null)
+    {
+        var operationResultOptions = new OperationResultOptions();
+        configuration?.Invoke(operationResultOptions);
+
+        services.TryAddSingleton(operationResultOptions);
+
+        return services;
+    }
+}
