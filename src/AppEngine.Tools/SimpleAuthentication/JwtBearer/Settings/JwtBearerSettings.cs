96772c919be752b3f0c1f0417e7bd41cce899948
Angelo Pirola <angelo9871@gmail.com>
2026-04-23 00:02:14 +0200

    Add JwtBearerSettings class for JWT auth configuration
    Introduced JwtBearerSettings to centralize JWT Bearer authentication configuration, including scheme, algorithm, security key, issuers, audiences, expiration, clock skew, and claim type settings.

---
+++ src/AppEngine.Tools/SimpleAuthentication/JwtBearer/Settings/JwtBearerSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AppEngine.Tools.SimpleAuthentication.JwtBearer.Settings;
+
+public class JwtBearerSettings
+{
+    public string SchemeName { get; set; } = JwtBearerDefaults.AuthenticationScheme;
+    public string Algorithm { get; set; } = SecurityAlgorithms.HmacSha256;
+    public string SecurityKey { get; set; } = null!;
+    public string[]? Issuers { get; set; }
+    public string[]? Audiences { get; set; }
+    public TimeSpan? ExpirationTime { get; set; }
+    public TimeSpan ClockSkew { get; set; } = TokenValidationParameters.DefaultClockSkew;
+    public string NameClaimType { get; set; } = ClaimsIdentity.DefaultNameClaimType;
+    public string RoleClaimType { get; set; } = ClaimsIdentity.DefaultRoleClaimType;
+}
