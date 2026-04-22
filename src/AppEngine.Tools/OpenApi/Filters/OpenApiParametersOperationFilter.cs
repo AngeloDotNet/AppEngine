1a2c95a0e338b4a47e55e6468451bd26f5851082
Angelo Pirola <angelo9871@gmail.com>
2026-04-23 00:02:02 +0200

    Add OpenApiParametersOperationFilter for custom parameters
    Introduced OpenApiParametersOperationFilter implementing IOpenApiOperationTransformer. This filter adds parameters from OpenApiOperationOptions to OpenApiOperation if they are not already present, enabling custom parameter injection into OpenAPI docs. Added relevant usings and namespace.

---
+++ src/AppEngine.Tools/OpenApi/Filters/OpenApiParametersOperationFilter.cs
@@ -0,0 +1,25 @@
+﻿using AppEngine.Tools.OpenApi.Options;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace AppEngine.Tools.OpenApi.Filters;
+
+internal class OpenApiParametersOperationFilter(OpenApiOperationOptions options) : IOpenApiOperationTransformer
+{
+    public Task TransformAsync(OpenApiOperation operation, OpenApiOperationTransformerContext context, CancellationToken cancellationToken)
+    {
+        if (options.Parameters.Count > 0)
+        {
+            operation.Parameters ??= [];
+
+            foreach (var parameter in options.Parameters
+                .Where(parameter => !operation.Parameters.Any(existingParameter
+                => existingParameter.Name == parameter.Name && existingParameter.In == parameter.In)))
+            {
+                operation.Parameters.Add(parameter);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
