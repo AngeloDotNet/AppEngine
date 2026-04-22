34ea26c8f67573ff49aa4c01c9aa95b75100827f
Angelo Pirola <angelo9871@gmail.com>
2026-04-23 00:01:42 +0200

    Add OpenApiExtensions for enhanced OpenAPI configuration
    Introduced OpenApiExtensions with methods to configure OpenAPI/Swagger in ASP.NET Core, including operation parameters, server list removal, Accept-Language header, default problem details, and simple authentication support. Registered relevant transformers and added required using directives.

---
+++ src/AppEngine.Tools/Extensions/OpenApiExtensions.cs
@@ -0,0 +1,52 @@
+﻿using AppEngine.Tools.OpenApi;
+using AppEngine.Tools.OpenApi.Filters;
+using AppEngine.Tools.OpenApi.Options;
+using AppEngine.Tools.OpenApi.SimpleAuthentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi;
+
+namespace AppEngine.Tools.Extensions;
+
+public static class OpenApiExtensions
+{
+    public static IServiceCollection AddOpenApiOperationParameters(this IServiceCollection services, Action<OpenApiOperationOptions> setupAction)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(setupAction);
+
+        var parameters = new OpenApiOperationOptions();
+        setupAction.Invoke(parameters);
+
+        services.AddTransient(_ => parameters);
+
+        return services;
+    }
+
+    extension(OpenApiOptions options)
+    {
+        public OpenApiOptions RemoveServerList() => options.AddDocumentTransformer<RemoveServerListDocumentTransformer>();
+        public OpenApiOptions AddAcceptLanguageHeader() => options.AddOperationTransformer<AcceptLanguageHeaderOperationTransformer>();
+        public OpenApiOptions AddDefaultProblemDetailsResponse() => options.AddOperationTransformer<DefaultResponseOperationTransformer>();
+        public OpenApiOptions AddOperationParameters() => options.AddOperationTransformer<OpenApiParametersOperationFilter>();
+    }
+
+    //public static void AddSimpleAuthentication(this OpenApiOptions options, IConfiguration configuration, string sectionName = "Authentication")
+    public static void AddSimpleAuthentication(this OpenApiOptions options, IConfiguration configuration, string sectionName)
+    {
+        options.AddSimpleAuthentication(configuration, sectionName, [], []);
+    }
+
+    public static void AddSimpleAuthentication(this OpenApiOptions options, IConfiguration configuration, string sectionName,
+        IEnumerable<OpenApiSecurityRequirement> addSecurityRequirements, IEnumerable<string> addSecurityDefinitionNames)
+    {
+        ArgumentNullException.ThrowIfNull(options);
+        ArgumentNullException.ThrowIfNull(configuration);
+        ArgumentException.ThrowIfNullOrWhiteSpace(sectionName);
+
+        options.AddDocumentTransformer(new AuthenticationDocumentTransformer(configuration, sectionName, addSecurityRequirements, addSecurityDefinitionNames));
+        options.AddDocumentTransformer<DefaultResponseDocumentTransformer>();
+        options.AddOperationTransformer<AuthenticationOperationTransformer>();
+    }
+}
