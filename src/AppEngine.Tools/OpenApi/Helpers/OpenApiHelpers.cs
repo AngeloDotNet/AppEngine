e8367a259e50e79f4eb07dc07ca93076eeb2b50e
Angelo Pirola <angelo9871@gmail.com>
2026-04-23 00:02:37 +0200

    Add OpenApiHelpers for security and response creation
    Introduced OpenApiHelpers static class with methods to create OpenApiSecurityRequirement and OpenApiResponse objects, streamlining OpenAPI integration. Utilizes references to security schemes and ProblemDetails schema. Added relevant using directives for required namespaces.

---
+++ src/AppEngine.Tools/OpenApi/Helpers/OpenApiHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mime;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi;
+
+namespace AppEngine.Tools.OpenApi.Helpers;
+
+internal static class OpenApiHelpers
+{
+    public static OpenApiSecurityRequirement CreateSecurityRequirement(string name, OpenApiDocument document)
+        => new()
+            {
+                { new OpenApiSecuritySchemeReference(name, document), [] }
+            };
+
+    public static OpenApiResponse CreateResponse(string description)
+        => new()
+        {
+            Description = description,
+            Content = new Dictionary<string, IOpenApiMediaType>
+            {
+                [MediaTypeNames.Application.ProblemJson] = new OpenApiMediaType()
+                {
+                    Schema = new OpenApiSchemaReference(nameof(ProblemDetails))
+                }
+            }
+            //Content = new Dictionary<string, OpenApiMediaType>
+            //{
+            //    [MediaTypeNames.Application.ProblemJson] = new()
+            //    {
+            //        Schema = new OpenApiSchemaReference(nameof(ProblemDetails))
+            //    }
+            //}
+        };
+}
