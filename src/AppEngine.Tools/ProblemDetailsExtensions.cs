7734efec5791371ff0714957a6ced65a1603079f
Angelo Pirola <angelo9871@gmail.com>
2026-04-24 00:08:32 +0200

    Refactor to C# 12 extension syntax; update .csproj
    Refactor extension methods to use C# 12 "extension" classes for improved clarity and maintainability. Update .csproj to include README in NuGet package and remove Microsoft.OpenApi dependency. No changes to method logic.

--- src/AppEngine.Tools/ProblemDetailsExtensions.cs
+++ src/AppEngine.Tools/ProblemDetailsExtensions.cs
@@ -7,25 +7,31 @@ namespace AppEngine.Tools;
 
 public static class ProblemDetailsExtensions
 {
-    public static IServiceCollection AddDefaultProblemDetails(this IServiceCollection services)
+    extension(IServiceCollection services)
     {
-        services.AddProblemDetails(options =>
+        public IServiceCollection AddDefaultProblemDetails()
         {
-            options.CustomizeProblemDetails = context => context.UseDefaults();
-        });
+            services.AddProblemDetails(options =>
+            {
+                options.CustomizeProblemDetails = context => context.UseDefaults();
+            });
 
-        return services;
+            return services;
+        }
     }
 
-    public static void UseDefaults(this ProblemDetailsContext context)
+    extension(ProblemDetailsContext context)
     {
-        var statusCode = context.ProblemDetails.Status.GetValueOrDefault(StatusCodes.Status500InternalServerError);
+        public void UseDefaults()
+        {
+            var statusCode = context.ProblemDetails.Status.GetValueOrDefault(StatusCodes.Status500InternalServerError);
 
-        context.ProblemDetails.Type ??= $"https://httpstatuses.io/{statusCode}";
-        context.ProblemDetails.Title ??= ReasonPhrases.GetReasonPhrase(statusCode);
-        context.ProblemDetails.Instance ??= context.HttpContext.Request.Path;
-        context.ProblemDetails.Detail ??= context.Exception?.Message;
+            context.ProblemDetails.Type ??= $"https://httpstatuses.io/{statusCode}";
+            context.ProblemDetails.Title ??= ReasonPhrases.GetReasonPhrase(statusCode);
+            context.ProblemDetails.Instance ??= context.HttpContext.Request.Path;
+            context.ProblemDetails.Detail ??= context.Exception?.Message;
 
-        context.ProblemDetails.Extensions.TryAdd("traceId", Activity.Current?.Id ?? context.HttpContext.TraceIdentifier);
+            context.ProblemDetails.Extensions.TryAdd("traceId", Activity.Current?.Id ?? context.HttpContext.TraceIdentifier);
+        }
     }
 }
