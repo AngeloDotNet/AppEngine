3c5e1b55cfde1cd4361eee410a16883f419eb96e
Angelo Pirola <angelo9871@gmail.com>
2026-04-24 00:08:05 +0200

    Add JwtBearerService and validation result class
    Implemented JwtBearerService for creating, validating, and refreshing JWT tokens with configurable settings. Added JwtBearerValidationResult to encapsulate token validation outcomes. Uses Microsoft.IdentityModel for secure token handling.

---
+++ src/AppEngine.Tools/SimpleAuthentication/JwtBearer/JwtBearerService.cs
@@ -0,0 +1,96 @@
+﻿using System.Security.Claims;
+using System.Text;
+using AppEngine.Tools.SimpleAuthentication.Abstractions;
+using AppEngine.Tools.SimpleAuthentication.JwtBearer.Interfaces;
+using AppEngine.Tools.SimpleAuthentication.JwtBearer.Settings;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AppEngine.Tools.SimpleAuthentication.JwtBearer;
+
+public class JwtBearerService(IOptions<JwtBearerSettings> jwtBearerSettingsOptions) : IJwtBearerService
+{
+    protected JwtBearerSettings JwtBearerSettings { get; } = jwtBearerSettingsOptions?.Value
+        ?? throw new ArgumentNullException(nameof(jwtBearerSettingsOptions));
+
+    public virtual Task<string> CreateTokenAsync(string userName, IList<Claim>? claims = null, string? issuer = null, string? audience = null,
+        DateTime? absoluteExpiration = null)
+    {
+        var now = DateTime.UtcNow;
+
+        if (absoluteExpiration.HasValue && absoluteExpiration.Value < now)
+        {
+            throw new ArgumentException("The expiration date must be greater than or equal to the current date and time.", nameof(absoluteExpiration));
+        }
+
+        claims ??= [];
+        claims.Update(JwtBearerSettings.NameClaimType, userName);
+        claims.Update(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString());
+
+        var securityTokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(claims, JwtBearerSettings.SchemeName, JwtBearerSettings.NameClaimType, JwtBearerSettings.RoleClaimType),
+            Issuer = issuer ?? JwtBearerSettings.Issuers?.FirstOrDefault(),
+            Audience = audience ?? JwtBearerSettings.Audiences?.FirstOrDefault(),
+            IssuedAt = now,
+            NotBefore = now.Add(-JwtBearerSettings.ClockSkew),
+            Expires = absoluteExpiration ?? (JwtBearerSettings.ExpirationTime.GetValueOrDefault() > TimeSpan.Zero ? now.Add(JwtBearerSettings.ExpirationTime!.Value) : DateTime.MaxValue),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtBearerSettings.SecurityKey)), JwtBearerSettings.Algorithm)
+        };
+
+        var tokenHandler = new JsonWebTokenHandler();
+        var token = tokenHandler.CreateToken(securityTokenDescriptor);
+
+        return Task.FromResult(token);
+    }
+
+    public virtual async Task<ClaimsPrincipal> ValidateTokenAsync(string token, bool validateLifetime = true)
+    {
+        var tokenHandler = new JsonWebTokenHandler();
+
+        if (!tokenHandler.CanReadToken(token))
+        {
+            throw new SecurityTokenException("Token is not a well formed Json Web Token (JWT)");
+        }
+
+        var tokenValidationParameters = new TokenValidationParameters
+        {
+            AuthenticationType = JwtBearerSettings.SchemeName,
+            NameClaimType = JwtBearerSettings.NameClaimType,
+            RoleClaimType = JwtBearerSettings.RoleClaimType,
+            ValidateIssuer = JwtBearerSettings.Issuers?.Any() ?? false,
+            ValidIssuers = JwtBearerSettings.Issuers,
+            ValidateAudience = JwtBearerSettings.Audiences?.Any() ?? false,
+            ValidAudiences = JwtBearerSettings.Audiences,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(JwtBearerSettings.SecurityKey)),
+            RequireExpirationTime = true,
+            ValidateLifetime = validateLifetime,
+            ClockSkew = JwtBearerSettings.ClockSkew
+        };
+
+        var validationResult = await tokenHandler.ValidateTokenAsync(token, tokenValidationParameters);
+
+        if (!validationResult.IsValid || validationResult.SecurityToken is not JsonWebToken jsonWebToken || jsonWebToken.Alg != JwtBearerSettings.Algorithm)
+        {
+            throw new SecurityTokenException("Token is expired or invalid", validationResult.Exception);
+        }
+
+        var principal = new ClaimsPrincipal(validationResult.ClaimsIdentity);
+        return principal;
+    }
+
+    public virtual async Task<string> RefreshTokenAsync(string token, bool validateLifetime, DateTime? absoluteExpiration = null)
+    {
+        var principal = await ValidateTokenAsync(token, validateLifetime);
+        var claims = (principal.Identity as ClaimsIdentity)!.Claims.ToList();
+
+        var userName = claims.First(c => c.Type == JwtBearerSettings.NameClaimType).Value;
+        var issuer = claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Iss)?.Value;
+        var audience = claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Aud)?.Value;
+
+        var newToken = await CreateTokenAsync(userName, claims, issuer, audience, absoluteExpiration);
+        return newToken;
+    }
+}
---
+++ src/AppEngine.Tools/SimpleAuthentication/JwtBearer/JwtBearerValidationResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Claims;
+
+namespace AppEngine.Tools.SimpleAuthentication.JwtBearer;
+
+public class JwtBearerValidationResult
+{
+    [MemberNotNullWhen(true, nameof(Principal))]
+    [MemberNotNullWhen(false, nameof(Exception))]
+    public bool IsValid { get; set; }
+    public ClaimsPrincipal? Principal { get; set; }
+    public Exception? Exception { get; set; }
+}
