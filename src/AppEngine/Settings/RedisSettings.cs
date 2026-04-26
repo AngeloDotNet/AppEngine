5a99809af24a6a22c1f4a2ef27bc39c0dc60f1fb
Angelo Pirola <angelo9871@gmail.com>
2026-04-26 16:30:25 +0200

    Add RedisSettings class for Redis configuration
    Introduced RedisSettings.cs in AppEngine.Settings namespace to encapsulate Redis configuration options such as instance name, endpoints, password, client name, keep-alive, SSL for Azure, and abort on connect fail, all with default values.

---
+++ src/AppEngine/Settings/RedisSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace AppEngine.Settings;
+
+public class RedisSettings
+{
+    public string InstanceName { get; set; } = string.Empty;
+    public string EndPoints { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string ClientName { get; set; } = string.Empty;
+    public int KeepAlive { get; set; } = 180;
+    public bool SslforAzure { get; set; } = false;
+    public bool AbortOnConnectFail { get; set; } = false;
+}
