4aa07981ddbd780dfc0f902653f2b308a8064ea4
Angelo Pirola <angelo9871@gmail.com>
2026-05-02 00:01:26 +0200

    Add validation options and DI extension methods
    Introduced AppEngine.Tools.Validation namespace with ValidationOptions class, an obsolete ErrorResponseFormat enum, and a ServiceCollectionExtensions class providing ConfigureValidation for DI configuration.

---
+++ src/AppEngine.Tools/Validation/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AppEngine.Tools.Validation;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection ConfigureValidation(Action<ValidationOptions> configureOptions)
+        {
+            services.Configure(configureOptions);
+            return services;
+        }
+    }
+}
