d0d6e04a58bcbdf1f9fe71b5b3a5d2b3065b178a
Angelo Pirola <angelo9871@gmail.com>
2026-05-02 00:01:13 +0200

    Remove validation options and related extension files
    Deleted ErrorResponseFormat.cs, ServiceCollectionExtensions.cs, and ValidationOptions.cs, removing the error response format enum, validation configuration extension, and validation options class. This cleans up unused validation infrastructure.

--- src/AppEngine/Validation/ValidationOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace AppEngine.Validation;
-
-public class ValidationOptions
-{
-    public ErrorResponseFormat ErrorResponseFormat { get; set; }
-    public Func<EndpointFilterInvocationContext, IDictionary<string, string[]>, string>? ValidationErrorTitleMessageFactory { get; set; }
-}
