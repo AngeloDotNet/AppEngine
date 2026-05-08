44fb06608d544c18f59d285ae5688f3454047b56
Angelo Pirola <angelo9871@gmail.com>
2026-05-08 08:50:20 +0200

    Add ServiceCollectionExtensions for DI and JSON config
    Added ServiceCollectionExtensions with methods to configure JSON serialization options and bind configuration sections to strongly-typed settings. Included commented-out code for future model validation support. Added required using directives.

---
+++ src/AppEngine.Tools/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System.Text.Json.Serialization;
+using AppEngine.Tools.Serialization;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AppEngine.Tools.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddHttpJsonOptions()
+        {
+            services.ConfigureHttpJsonOptions(options =>
+            {
+                options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+                options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.SerializerOptions.Converters.Add(new UtcDateTimeConverter());
+            });
+
+            return services;
+        }
+
+        public T? ConfigureAndGet<T>(IConfiguration configuration, string sectionName) where T : class
+        {
+            ArgumentNullException.ThrowIfNull(services);
+            ArgumentNullException.ThrowIfNull(configuration);
+            ArgumentNullException.ThrowIfNull(sectionName);
+
+            var section = configuration.GetSection(sectionName);
+            services.Configure<T>(section);
+
+            var settings = Activator.CreateInstance<T>();
+            section.Bind(settings);
+
+            return settings;
+        }
+    }
+
+    //extension(RouteHandlerBuilder builder)
+    //{
+    //    public RouteHandlerBuilder WithValidation<TModel>() where TModel : class
+    //    {
+    //        builder.AddEndpointFilter<ValidatorFilter<TModel>>().ProducesValidationProblem();
+
+    //        return builder;
+    //    }
+    //}
+}
