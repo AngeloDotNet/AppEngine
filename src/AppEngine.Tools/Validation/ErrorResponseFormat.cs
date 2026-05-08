a5099d13b62d7a7ce9ed9150efa6b544e6a11a1e
Angelo Pirola <angelo9871@gmail.com>
2026-05-08 08:51:01 +0200

    Remove obsolete validation config and error format code
    Removed ErrorResponseFormat enum, ValidationOptions class, and ServiceCollectionExtensions from AppEngine.Tools.Validation. These components are no longer needed due to changes in how validation and error responses are handled.

--- src/AppEngine.Tools/Validation/ErrorResponseFormat.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppEngine.Tools.Validation;
-
-//[Obsolete("This enum is no longer used and will be removed in a future version. " +
-//    "The error response format is now determined by the presence of the OperationResultList<T> type in the response.", false)]
-//public enum ErrorResponseFormat
-//{
-//    Default,
-//    List
-//}
