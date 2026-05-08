e68ff9261ff00735f7f05bb2fefd6afd9b193cca
Angelo Pirola <angelo9871@gmail.com>
2026-05-08 08:51:54 +0200

    Add validation options and error response format support
    Introduced ErrorResponseFormat enum and ValidationOptions class for flexible validation configuration. Added ServiceCollectionExtensions with ConfigureValidation extension method for DI registration.

---
+++ src/AppEngine.Validations/Validation/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AppEngine.Validations.Validation;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection ConfigureValidation(Action<ValidationOptions> configureOptions)
+        {
+            services.Configure(configureOptions);
+            return services;
+        }
+    }
+}
---
+++ src/AppEngine.Validations/Validation/ValidationOptions.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace AppEngine.Validations.Validation;
+
+public class ValidationOptions
+{
+    public ErrorResponseFormat ErrorResponseFormat { get; set; }
+    public Func<EndpointFilterInvocationContext, IDictionary<string, string[]>, string>? ValidationErrorTitleMessageFactory { get; set; }
+}
