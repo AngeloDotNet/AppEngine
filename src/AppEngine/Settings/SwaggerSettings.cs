aff099be4b086d037fb77e397c504692bd334a69
Angelo Pirola <angelo9871@gmail.com>
2026-05-16 11:20:23 +0200

    Remove API versioning and Swagger auth middleware
    Deleted all API versioning and Swagger basic authentication files, including related transformers and settings. Commented out Asp.Versioning package references in the project file to fully remove custom versioning and Swagger authentication logic.

--- src/AppEngine/Settings/SwaggerSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppEngine.Settings;
-
-public class SwaggerSettings
-{
-    public bool IsEnabled { get; set; }
-    public bool RequireAuthentication { get; set; }
-    public string? UserName { get; set; }
-    public string? Password { get; set; }
-}
