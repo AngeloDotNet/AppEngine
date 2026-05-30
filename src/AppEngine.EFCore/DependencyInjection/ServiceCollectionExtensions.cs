2dae2d5f6d70df900c73fa2e64d60c5932bb4813
Angelo Pirola <angelo9871@gmail.com>
2026-05-30 10:01:48 +0200

    Add DI extensions for EF Core repositories and services
    Introduced ServiceCollectionExtensions with methods to register repositories, unit of work, interceptors, and a migration helper for EF Core. Added necessary using directives and extension methods for IServiceCollection and IHost.

---
+++ src/AppEngine.EFCore/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using AppEngine.EFCore.Entities.Interfaces;
+using AppEngine.EFCore.Interceptors;
+using AppEngine.EFCore.Repositories;
+using AppEngine.EFCore.Repositories.Interfaces;
+using AppEngine.EFCore.Services;
+using AppEngine.EFCore.Services.Interfaces;
+using AppEngine.EFCore.UoW;
+using AppEngine.EFCore.UoW.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AppEngine.EFCore.DependencyInjection;
+
+public static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddPersistence<TEntity, TKey, TDbContext>()
+        where TEntity : class, IEntity<TKey>
+        where TKey : IEquatable<TKey>
+        where TDbContext : DbContext
+        {
+            services.AddScoped<IRepository<TEntity, TKey>, Repository<TEntity, TKey, TDbContext>>();
+
+            return services;
+        }
+
+        public IServiceCollection AddPersistenceUoW<TEntity, TKey, TDbContext>()
+            where TEntity : class, IEntity<TKey>
+            where TKey : IEquatable<TKey>
+            where TDbContext : DbContext
+        {
+            services.AddScoped<IRepository<TEntity, TKey>, Repository<TEntity, TKey, TDbContext>>();
+            services.AddScoped<IUnitOfWork, UnitOfWork<TDbContext>>();
+
+            return services;
+        }
+
+        public IServiceCollection AddInterceptors()
+        {
+            services.AddScoped<AuditSaveChangesInterceptor>();
+            services.AddScoped<SoftDeleteInterceptor>();
+
+            services.AddScoped<ICurrentUserService, CurrentUserService>();
+
+            return services;
+        }
+    }
+
+    extension(IHost app)
+    {
+        public void ApplyMigrations<TDbContext>() where TDbContext : DbContext
+        {
+            using var scope = app.Services.CreateScope();
+
+            var dbContext = scope.ServiceProvider.GetRequiredService<TDbContext>();
+            dbContext.Database.Migrate();
+        }
+    }
+}
