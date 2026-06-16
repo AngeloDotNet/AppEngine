c66c06cce9a3fcab13b5679032a093479d233a4a
Angelo Pirola <angelo9871@gmail.com>
2026-06-16 08:50:51 +0200

    Add OAuth2 transformer for OpenAPI document updates
    Introduced OAuth2AuthenticationDocumentTransformer to inject OAuth2 (Authorization Code flow) security schemes and requirements into OpenAPI documents. Included helper methods for managing security requirements and updated necessary imports.

---
+++ src/AppEngine.Tools/OpenApi/SimpleAuthentication/OAuth2AuthenticationDocumentTransformer.cs
@@ -0,0 +1,36 @@
+﻿using AppEngine.Tools.OpenApi.Helpers;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace AppEngine.Tools.OpenApi.SimpleAuthentication;
+
+internal class OAuth2AuthenticationDocumentTransformer(string name, OpenApiOAuthFlow authFlow) : IOpenApiDocumentTransformer
+{
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        document.Components ??= new();
+        document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+
+        document.Components.SecuritySchemes.Add(name, new OpenApiSecurityScheme()
+        {
+            Type = SecuritySchemeType.OAuth2,
+            Flows = new()
+            {
+                AuthorizationCode = authFlow
+            }
+        });
+
+        AddSecurityRequirement(document, name);
+
+        return Task.CompletedTask;
+    }
+
+    internal static void AddSecurityRequirement(OpenApiDocument document, string name)
+        => AddSecurityRequirement(document, OpenApiHelpers.CreateSecurityRequirement(name, document));
+
+    private static void AddSecurityRequirement(OpenApiDocument document, OpenApiSecurityRequirement requirement)
+    {
+        document.Security ??= [];
+        document.Security.Add(requirement);
+    }
+}
